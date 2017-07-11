e45706ae1e0e1eed37e6cdec3f3f7d66ff6bf24a
hdnes <rbusenet@gmail.com>
2017-07-11 16:02:15 -0700

    Corrected packet send order in comments

--- pyduml.py
+++ pyduml.py
@@ -12,6 +12,9 @@ packet_1 = bytearray.fromhex(u'55 16 04 FC 2A 28 65 57 40 00 07 00 00 00 00 00 0
 # Enable Reporting
 packet_2 = bytearray.fromhex(u'55 0E 04 66 2A 28 68 57 40 00 0C 00 88 20')
 
+# ToDo: Send packets 1,2 above via pyusb raw packet transfer
+# ToDo: put ftp file transfer here! /ftp/upgrade/xxx_system.bin
+
 # 551A04B12A286B5740000800YYYYYYYY0000000000000204XXXX
 #YYYYYYYY - file size in little endian
 #XXXX - CRC as produced by dji_crc.py
@@ -41,8 +44,6 @@ hex_data = filehash.decode("hex")
 md5_check = bytearray(hex_data)
 print ' '.join(format(x, '02X') for x in md5_check)
 
-# ToDo: Send packets 1,2,3 above via pyusb raw packet transfer
-# ToDo: put ftp file transfer here! /ftp/upgrade/xxx_system.bin
 
 # File Verification and Start Upgrade
 packet_4 = bytearray.fromhex(u'55 1E 04 8A 2A 28 F6 57 40 00 0A 00')
@@ -55,6 +56,6 @@ crc = struct.pack('<H',crc) #need to check endianess?
 packet_4 += crc
 print ' '.join(format(x, '02X') for x in packet_4)
 
-# ToDo: Send packet 4 via pyusb raw packet transfer
+# ToDo: Send packet 3,4 via pyusb raw packet transfer
 
 
