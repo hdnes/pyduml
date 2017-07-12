eb88409336ed941d0f60de7870682f47fabba945
hdnes <rbusenet@gmail.com>
2017-07-12 16:34:49 -0700

    Functionize previous packet generation work and create main loop

--- pyduml.py
+++ pyduml.py
@@ -1,59 +1,75 @@
 #!/usr/bin/python
 # HDnes pythonDUML
 # Thanks the_lord for the sniffing
-import os
+# Thanks jaydee for the usb and ftp work
 
-from table_crc import *
+import os
+import sys
+import usb.core
+import usb.util
 import struct
 import hashlib
-
-# Enter upgrade mode (delete old file if exists)
-packet_1 = bytearray.fromhex(u'55 16 04 FC 2A 28 65 57 40 00 07 00 00 00 00 00 00 00 00 00 27 D3')
-# Enable Reporting
-packet_2 = bytearray.fromhex(u'55 0E 04 66 2A 28 68 57 40 00 0C 00 88 20')
+from ftplib import FTP
+from table_crc import *
 
 # ToDo: Send packets 1,2 above via pyusb raw packet transfer
 # ToDo: put ftp file transfer here! /ftp/upgrade/xxx_system.bin
-
-# 551A04B12A286B5740000800YYYYYYYY0000000000000204XXXX
-#YYYYYYYY - file size in little endian
-#XXXX - CRC as produced by table_crc.py
-
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "/dji_system.bin"
-# Pack file size into 4 byte Long little endian
-file_size = struct.pack('<L',int(os.path.getsize(dir_path)))
-
-packet_3 = bytearray.fromhex(u'55 1A 04 B1 2A 28 6B 57 40 00 08 00')
-packet_3 += file_size #append file size
-packet_3 += bytearray.fromhex(u'00 00 00 00 00 00 02 04')
-
-packet_length = len(packet_3)
-crc = calc_checksum(packet_3,packet_length)
-crc = struct.pack('<H',crc)
-packet_3 += crc
-
-
-# Calculate File md5 hash
-filehash = hashlib.md5()
-filehash.update(open(dir_path).read())
-filehash = filehash.hexdigest()
-hex_data = filehash.decode("hex")
-md5_check = bytearray(hex_data)
-
-
-# File Verification and Start Upgrade
-packet_4 = bytearray.fromhex(u'55 1E 04 8A 2A 28 F6 57 40 00 0A 00')
-packet_4 += md5_check
-
-packet_length = len(packet_4)
-crc = struct.pack('<H',crc) #need to check endianess?
-packet_4 += crc
-
-print ' '.join(format(x, '02X') for x in packet_1)
-print ' '.join(format(x, '02X') for x in packet_2)
-print ' '.join(format(x, '02X') for x in packet_3)
-print ' '.join(format(x, '02X') for x in packet_4)
-
 # ToDo: Send packet 3,4 via pyusb raw packet transfer
 
+def main():
+	generate_update_packets()
 
+	return;
+
+def generate_update_packets():
+	# Enter upgrade mode (delete old file if exists)
+	packet_1 = bytearray.fromhex(u'55 16 04 FC 2A 28 65 57 40 00 07 00 00 00 00 00 00 00 00 00 27 D3')
+	# Enable Reporting
+	packet_2 = bytearray.fromhex(u'55 0E 04 66 2A 28 68 57 40 00 0C 00 88 20')
+
+
+	# 551A04B12A286B5740000800YYYYYYYY0000000000000204XXXX
+	#YYYYYYYY - file size in little endian
+	#XXXX - CRC as produced by table_crc.py
+
+	dir_path = os.path.dirname(os.path.realpath(__file__)) + "/dji_system.bin"
+	# Pack file size into 4 byte Long little endian
+	file_size = struct.pack('<L',int(os.path.getsize(dir_path)))
+
+	packet_3 = bytearray.fromhex(u'55 1A 04 B1 2A 28 6B 57 40 00 08 00')
+	packet_3 += file_size #append file size
+	packet_3 += bytearray.fromhex(u'00 00 00 00 00 00 02 04')
+
+	packet_length = len(packet_3)
+	crc = calc_checksum(packet_3,packet_length)
+	crc = struct.pack('<H',crc)
+	packet_3 += crc
+
+
+	# Calculate File md5 hash
+	filehash = hashlib.md5()
+	filehash.update(open(dir_path).read())
+	filehash = filehash.hexdigest()
+	hex_data = filehash.decode("hex")
+	md5_check = bytearray(hex_data)
+
+
+	# File Verification and Start Upgrade
+	packet_4 = bytearray.fromhex(u'55 1E 04 8A 2A 28 F6 57 40 00 0A 00')
+	packet_4 += md5_check
+
+	packet_length = len(packet_4)
+	crc = calc_checksum(packet_4,packet_length)
+	crc = struct.pack('<H',crc)
+	packet_4 += crc
+
+	print ' '.join(format(x, '02X') for x in packet_1)
+	print ' '.join(format(x, '02X') for x in packet_2)
+	print ' '.join(format(x, '02X') for x in packet_3)
+	print ' '.join(format(x, '02X') for x in packet_4)
+	return;
+
+
+
+if __name__ == "__main__":
+    main()
