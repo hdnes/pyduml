d7885cbcc416cb11fa3e9e0d9b97d381ce3ec4b8
hdnes <rbusenet@gmail.com>
2017-07-15 19:10:05 -0700

    Added Support for Mavic RC (un-tested), Cleaned up FTP

--- pyduml.py
+++ pyduml.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+
 # HDnes pythonDUML
+# Thanks Hostile for the fireworks grepping all the fish.
 # Thanks the_lord for the sniffing
 # Thanks jaydee for the usb and ftp work
 
@@ -11,18 +13,24 @@ import usb.core
 import usb.util
 import struct
 import hashlib
-from ftplib import FTP
 from table_crc import *
 
-# ToDo: Send packets 1,2 above via pyusb raw packet transfer
-# ToDo: put ftp file transfer here! /ftp/upgrade/xxx_system.bin
-# ToDo: Send packet 3,4 via pyusb raw packet transfer
+global aircraft
+print ("---------------------------------------------------------------")
+aircraft = input('Select device number as follows: MavicPro = [1], Mavic RC = [2] : ')
+print ("---------------------------------------------------------------")
+if aircraft==1:
+    print ("Running Exploit for Mavic Pro")
+else:
+    print ("Running Exploit for Mavic RC")
+print ("---------------------------------------------------------------")
 
 def main():
+   
     #probe_for_device()
     configure_usb()
     generate_update_packets()
-    write_packet(packet_1) # Enter upgrade mode (delete old file if exists)	
+    write_packet(packet_1) # Enter upgrade mode (delete old file if exists) 
     write_packet(packet_2) # Enable Reporting
     upload_binary()
     write_packet(packet_3) # Send File size
@@ -48,15 +56,18 @@ def probe_for_device():
 
 def configure_usb():
     #serial.tools.list_ports
+
+    # Serial Port should resemble: '/dev/cu.usbmodem1425'
     global ser
     ser = serial.Serial(sys.argv[1])
     ser.baudrate = 115200  
-    #data_bits = 8  
-    #stop_bits = 1  
-    #parity = SerialPort::NONE
+        #data_bits = 8  
+        #stop_bits = 1  
+        #parity = SerialPort::NONE
+
     return
 
-def write_packet(data):		
+def write_packet(data):     
     ser.write(data)     # write a string
     time.sleep(0.1)
     hexout = ' '.join(format(x, '02X') for x in data)
@@ -72,58 +83,95 @@ def upload_binary():
         ftp = FTP("192.168.42.2", "Gimme", "DatROot!")
         fh = open("dji_system.bin", 'rb')
         ftp.storbinary('STOR /upgrade/dji_system.bin', fh)
-        ftp.mkdir("/upgrade/.bin")
+
+        if aircraft == 2: # Is this only for Mavic RC? 505 Permission error on Mavic Pro
+            ftp.mkdir("/upgrade/.bin")
+
         fh.close()
-    return	
+        ftp.quit()
+    return  
 
 def generate_update_packets():
-    # Enter upgrade mode (delete old file if exists)
+    
     global packet_1 
     global packet_2 
     global packet_3 
     global packet_4
 
-    packet_1 = bytearray.fromhex(u'55 16 04 FC 2A 28 65 57 40 00 07 00 00 00 00 00 00 00 00 00 27 D3')
-    # Enable Reporting
-    packet_2 = bytearray.fromhex(u'55 0E 04 66 2A 28 68 57 40 00 0C 00 88 20')
-
-
-    # 551A04B12A286B5740000800YYYYYYYY0000000000000204XXXX
-    #YYYYYYYY - file size in little endian
-    #XXXX - CRC as produced by table_crc.py
-
     dir_path = os.path.dirname(os.path.realpath(__file__)) + "/dji_system.bin"
     # Pack file size into 4 byte Long little endian
     file_size = struct.pack('<L',int(os.path.getsize(dir_path)))
 
-    packet_3 = bytearray.fromhex(u'55 1A 04 B1 2A 28 6B 57 40 00 08 00')
-    packet_3 += file_size #append file size
-    packet_3 += bytearray.fromhex(u'00 00 00 00 00 00 02 04')
+    if aircraft == 1: #Mavic Pro
+        # Enter upgrade mode (delete old file if exists)
+        packet_1 = bytearray.fromhex(u'55 16 04 FC 2A 28 65 57 40 00 07 00 00 00 00 00 00 00 00 00 27 D3')
+        # Enable Reporting
+        packet_2 = bytearray.fromhex(u'55 0E 04 66 2A 28 68 57 40 00 0C 00 88 20')
 
-    packet_length = len(packet_3)
-    crc = calc_checksum(packet_3,packet_length)
-    crc = struct.pack('<H',crc)
-    packet_3 += crc
+        # 551A04B12A286B5740000800YYYYYYYY0000000000000204XXXX
+        #YYYYYYYY - file size in little endian
+        #XXXX - CRC as produced by table_crc.py
 
+        packet_3 = bytearray.fromhex(u'55 1A 04 B1 2A 28 6B 57 40 00 08 00')
+        packet_3 += file_size #append file size
+        packet_3 += bytearray.fromhex(u'00 00 00 00 00 00 02 04')
 
-    # Calculate File md5 hash
-    filehash = hashlib.md5()
-    filehash.update(open(dir_path).read())
-    filehash = filehash.hexdigest()
-    hex_data = filehash.decode("hex")
-    md5_check = bytearray(hex_data)
+        packet_length = len(packet_3)
+        crc = calc_checksum(packet_3,packet_length)
+        crc = struct.pack('<H',crc)
+        packet_3 += crc
 
+        # Calculate File md5 hash
+        filehash = hashlib.md5()
+        filehash.update(open(dir_path).read())
+        filehash = filehash.hexdigest()
+        hex_data = filehash.decode("hex")
+        md5_check = bytearray(hex_data)
 
-    # File Verification and Start Upgrade
-    packet_4 = bytearray.fromhex(u'55 1E 04 8A 2A 28 F6 57 40 00 0A 00')
-    packet_4 += md5_check
+        # File Verification and Start Upgrade
+        packet_4 = bytearray.fromhex(u'55 1E 04 8A 2A 28 F6 57 40 00 0A 00')
+        packet_4 += md5_check
 
-    packet_length = len(packet_4)
-    crc = calc_checksum(packet_4,packet_length)
-    crc = struct.pack('<H',crc)
-    packet_4 += crc
+        packet_length = len(packet_4)
+        crc = calc_checksum(packet_4,packet_length)
+        crc = struct.pack('<H',crc)
+        packet_4 += crc
+    
+    elif aircraft == 2: #Mavic RC
+        # Enter upgrade mode (delete old file if exists)
+        packet_1 = bytearray.fromhex(u'55 16 04 FC 2A 2D E7 27 40 00 07 00 00 00 00 00 00 00 00 00 9F 44')
+        # Enable Reporting
+        packet_2 = bytearray.fromhex(u'55 0E 04 66 2A 2D EA 27 40 00 0C 00 2C C8')
+
+        packet_3 = bytearray.fromhex(u'55 1A 04 B1 2A 2D EC 27 40 00 08 00')
+        packet_3 += file_size #append file size
+        packet_3 += bytearray.fromhex(u'00 00 00 00 00 00 02 04')
+
+        packet_length = len(packet_3)
+        crc = calc_checksum(packet_3,packet_length)
+        crc = struct.pack('<H',crc)
+        packet_3 += crc
+
+        # Calculate File md5 hash
+        filehash = hashlib.md5()
+        filehash.update(open(dir_path).read())
+        filehash = filehash.hexdigest()
+        hex_data = filehash.decode("hex")
+        md5_check = bytearray(hex_data)
+
+        # File Verification and Start Upgrade
+        packet_4 = bytearray.fromhex(u'55 1E 04 8A 2A 2D 02 28 40 00 0A 00')
+        packet_4 += md5_check
+
+        packet_length = len(packet_4)
+        crc = calc_checksum(packet_4,packet_length)
+        crc = struct.pack('<H',crc)
+        packet_4 += crc
+
+    else:
+        print ("You picked an option not yet supported")
+
     return
-
 
 
 if __name__ == "__main__":
